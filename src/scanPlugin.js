c77c5a4c8f868f153e71e43294a21d84d7f96fc3
Stone Gray <stone@stonegray.ca>
2020-11-19 01:32:33 -0500

    Use improved Mach-O header parser

--- src/scanPlugin.js
+++ src/scanPlugin.js
@@ -84,19 +84,39 @@ export async function scanPlugin(plugin) {
 	// Detect architecture:
 	const executable = plist.CFBundleExecutable;
 	const executablePath = path.join(plugin.dir, plugin.name, './Contents/MacOS/', executable);
-	const header = await getHeaderBytes(executablePath, 16);
-	if (Buffer.compare(header, Buffer.from('CFFAEDFE', 'hex'))) {
-		info.arch = ['x64'];
-	} else if (Buffer.compare(header, Buffer.from('CEFAEDFE', 'hex'))) {
-		info.arch = ['x32'];
-	} else if (Buffer.compare(header, Buffer.from('CAFEBABE', 'hex'))) {
-		// Universal/Object file
-		info.arch = ['x32', 'x64'];
-	} else {
-		// TODO: Should add aarch64 header parser before Ableton releases an macOS/ARM version
-		info.type = 'Unknown/Malformed';
-		info.ok = false;
-		info.errors.push('Unsupported architecture', header.toString('hex'));
+	const header = await getHeaderBytes(executablePath, 8);
+	const headerBytesHex = header.toString('hex');
+
+	// Architecture values defined here:
+	// https://opensource.apple.com/source/cctools/cctools-836/include/mach/machine.h
+
+	// First int (magic) is defined here:
+	// https://opensource.apple.com/source/xnu/xnu-1456.1.26/EXTERNAL_HEADERS/mach-o/loader.h
+
+	switch (headerBytesHex) {
+
+	// MH_CIGAM_64, CPU_TYPE_I386 | CPU_ARCH_ABI64
+	case 'cffaedfe07000001':
+		// MH_MAGIC_64, CPU_TYPE_I386 | CPU_ARCH_ABI64
+	case 'cafebabe07000001':
+		info.arch = ['x64'];	
+		break;
+
+		// MH_CIGAM, CPU_TYPE_I386
+	case 'cefaedfe07000000':
+		// MH_MAGIC, CPU_TYPE_I386
+	case 'cafebabe07000000':
+		info.arch = ['x32'];	
+		break;
+
+		// MH_CIGAM_64, CPU_TYPE_ARM | CPU_ARCH_ABI64
+	case 'cffaedfe12000001':
+		info.arch = ['arm64'];	
+		break;
+		
+	default:
+		info.errors.push('Unknown architecture: '+headerBytesHex.toUpperCase);
+		break;
 	}
 
 	// AudioCompoents can (and many do) have a number of effects/devices
